f8e1ff16105a1d8c00c98f9cdd761d193c0b3312
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2020-07-16 09:59:26 +0200

    Add docstrings

--- exoplanets/astro_data.py
+++ exoplanets/astro_data.py
@@ -36,7 +36,34 @@ DEFAULT_PARAMS = {
 
 
 def read_kepler_data(table, columns=None, where=None, filename=None):
-    """
+    """ Returns a pandas DataFrame containing the requested data from the
+        NasaExoplanetArchive API described in [1].
+
+        :param str table:
+            The name of the table in the NasaExoplanetArchive API. The list of tables
+            is provided in [1].
+        :type columns: list or dict
+        :param columns:
+            The list, or dict containing the name of the columns to read from the table.
+            It is possible to select all columns from the table (columns=["*"]), or
+            a subset of the columns (e.g. columns=["dist", "mass"]). If columns is
+            None, then the default subset of columns for the table is used if defined
+            in DEFAULT_PARAMS. If columns is a dictionary, the keys are pulled from
+            the table and are rename into the values in the dictionary.
+        :param str where:
+            The where filter by which to filter the query (e.g. where="kepid=12345"
+            or "st_quarters like '%1%'"). If where is None, then the default
+            'where' filter for the table is used if defined in DEFAULT_PARAMS,
+            else no filter is applied.
+        :param str filename:
+            If a filename is provided, the pandas DataFrame is saved in .csv format if
+            the file does not exist, and is directly read from the file if it already
+            exists.
+
+        :returns pandas.DataFrame:
+            The pandas DataFrame containing the requested NasaExoplanetArchive table.
+
+        [1]: https://exoplanetarchive.ipac.caltech.edu/docs/program_interfaces.html
     """
     columns, where = get_default_params(table, columns, where)
     if not filename or not os.path.isfile(filename):
@@ -52,7 +79,19 @@ def read_kepler_data(table, columns=None, where=None, filename=None):
 
 
 def get_default_params(table, columns, where):
-    """
+    """ Returns the default columns and where parameters for the table if specified in
+        DEFAULT_PARAMS and if 'columns' and/or 'where' are None.
+
+        :param str table:
+            The name of the table.
+        :type columns: list or dict
+        :param columns:
+            The columns to read from the table.
+        :param str where:
+            The 'where' filter to apply to the table.
+
+        :returns tuple:
+            A tuple containing the columns and where parameters.
     """
     if table in DEFAULT_PARAMS:
         columns = DEFAULT_PARAMS[table]["columns"] if not columns else columns
@@ -61,7 +100,19 @@ def get_default_params(table, columns, where):
 
 
 def get_kepler_data(table, columns, where):
-    """
+    """ Returns an astropy table containing the requested data from the
+        NasaExoplanetArchive API.
+
+        :param str table:
+            The name of the table in the NasaExoplanetArchive API.
+        :type columns: list or dict
+        :param columns:
+            The columns to read from the table.
+        :param str where:
+            The 'where' filter to apply to the table.
+
+        :returns astropy.table.QTable:
+            The astropy table containing the requested data.
     """
     if isinstance(columns, dict):
         columns = [col for col in columns]
@@ -72,7 +123,19 @@ def get_kepler_data(table, columns, where):
 
 
 def rename_columns(df, columns):
-    """
+    """ Renames the pandas DataFrame columns if the 'columns' parameters is a
+        dictionary.
+
+        :param pandas.DataFrame df:
+            A pandas DataFrame to rename.
+        :type columns: list or dict
+        :param columns:
+            The columns to rename if provided in the form of a dictionary. In this
+            cases, the DataFrame columns which are keys in the dictionary are renamed
+            by the dictionary values.
+
+        :returns pandas.DataFrame:
+            The renamed pandas DataFrame.
     """
     if isinstance(columns, dict):
         df = df.rename(columns=columns)
@@ -80,7 +143,12 @@ def rename_columns(df, columns):
 
 
 def record_dataframe(df, filename):
-    """
+    """ Saves the pandas DataFrame into a .csv format if a filename is provided.
+
+        :param pandas.DataFrame df:
+            A pandas DataFrame to save into a .csv format.
+        :param str filename:
+            The location in which to save the DataFrame into a .csv format.
     """
     if filename:
         df.to_csv(filename, index=False)
